2f6b4467a4479127b5d1e8f23a894c7fb44769ca
Andri Danusasmita <andridsasmita@gmail.com>
2018-08-21 14:39:58 +0900

    Files reordering

--- rename-whitespace2under.sh
+++
@@ -1,6 +0,0 @@
-for f in *
-do
-echo "original: ${f}"
-echo "renamed to:  ${f//[ ()\[\]]/_}"
-mv "${f}" "${f//[ ()\[\]]/_}"
-done
--- twitchstream.sh
+++
@@ -1,4 +0,0 @@
-# watching twitch stream using streamlink & twitch
-pip3 install --user streamlink
-streamlink -p mpv twitch.tv/openai 1080p60
-
